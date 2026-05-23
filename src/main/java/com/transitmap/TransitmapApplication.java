3a74110d20130a09048d2406de2e2995185b64e9
med salime <medsalime66@gmail.com>
2026-05-23 23:46:54 +0000

    first commit

---
+++ src/main/java/com/transitmap/TransitmapApplication.java
@@ -0,0 +1,13 @@
+package com.transitmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransitmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransitmapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/transitmap/TransitmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transitmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransitmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
